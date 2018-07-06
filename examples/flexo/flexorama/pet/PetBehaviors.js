e1ac5a1d654d7ab94cc6d207c089cbc0779102b1
flexo <f37314b1@opayq.com>
2018-07-06 15:30:29 -0400

    asddddddasddd

--- examples/flexo/flexorama/pet/PetBehaviors.js
+++ examples/flexo/flexorama/pet/PetBehaviors.js
@@ -1,26 +1,43 @@
-Follower = function(_followTarget, minDistance, maxDistance) {
+Follower = function(followTarget, minDistance, maxDistance, moveSpeed) {
 
-    this.followTarget = _followTarget;
+    this.followTarget = followTarget;
+    this.minDistance = minDistance;
+    this.maxDistance = maxDistance;
+    this.moveSpeed = moveSpeed;
+
+    this.move = false;
 
     this.awake = function(parent, scene) {
         this.object3d = parent;
     }
 
     this.update = function(deltaTime) {
-        // get distance to target
-        // if target distance > maxdistance
-        // var f = GetDistance(this.object3d.position, this.followTarget.position);
-        // console.log("Distance" + f);
-        // console.log("follower: " + this.object3d.position);
-        // if(this.followTarget)
-        //     console.log("this.followTarget: " + this.followTarget.position);
-
-        if(this.followTarget)
-            this.object3d.position.set(
-                this.followTarget.position.x, 
-                0.25, 
-                this.followTarget.position.z
+        if(this.followTarget) {
+            var dist = GetDistance(
+                this.object3d.position,
+                this.followTarget.position
             );
+
+            if(dist > maxDistance)
+                this.move = true;
+            if(dist <= minDistance)
+                this.move = false;
+
+            if(this.move) {
+                var moveDir = GetMoveDirection(
+                    this.object3d.position,
+                    this.followTarget.position
+                );
+                this.object3d.position.x = moveDir.x * this.moveSpeed * deltaTime;
+                this.object3d.position.z = moveDir.z * this.moveSpeed * deltaTime;
+            }
+        }
+
+        // this.object3d.position.set(
+        //     this.followTarget.position.x, 
+        //     0.25, 
+        //     this.followTarget.position.z
+        // );
     }
 
     function GetDistance(v1, v2) {
@@ -28,6 +45,10 @@ Follower = function(_followTarget, minDistance, maxDistance) {
         var dy = v1.y - v2.y;
         var dz = v1.z - v2.z;
         return Math.sqrt(dx*dx + dy*dy + dz*dz);
+    }
+
+    function GetMoveDirection(v1, v2) {
+        return v2 - v1;
     }
 
 }
