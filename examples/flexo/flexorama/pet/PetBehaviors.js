5115384e84c4f58efa7c141fda18cab69b132eb5
flexo <f37314b1@opayq.com>
2018-07-06 12:20:34 -0400

    asd

--- examples/flexo/flexorama/pet/PetBehaviors.js
+++ examples/flexo/flexorama/pet/PetBehaviors.js
@@ -9,6 +9,15 @@ Follower = function(followTarget, minDistance, maxDistance) {
     this.update = function(deltaTime) {
         // get distance to target
         // if target distance > maxdistance
+        var f = GetDistance(this.object3d.position, this.followTarget.position);
+        console.log(f);
+    }
+
+    function GetDistance(v1, v2) {
+        var dx = v1.x - v2.x;
+        var dy = v1.y - v2.y;
+        var dz = v1.z - v2.z;
+        return Math.sqrt(dx*dx + dy*dy + dz*dz);
     }
 
 }
