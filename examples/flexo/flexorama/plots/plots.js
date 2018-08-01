f727a4c56ae5e715622e128e4befa621ae1238cf
flexo <f37314b1@opayq.com>
2018-08-01 08:51:41 -0400

    asdd

--- examples/flexo/flexorama/plots/plots.js
+++ examples/flexo/flexorama/plots/plots.js
@@ -63,13 +63,10 @@ RotateY = function(rate) {
     }
 }
 
-
 PlotUserInfo = function(head) {
-
     this.awake = function(parent, scene) {
         this.object3d = parent;
     }
-
     this.update = function(deltaTime) {
         if(head) console.log(head.position);
     }
